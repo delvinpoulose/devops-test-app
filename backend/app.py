0c7780eac38e44d7532ea1432e09101973b09e7a
delvinpoulose <31891852+delvinpoulose@users.noreply.github.com>
2025-04-28 07:30:03 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -1,10 +1,10 @@
 from fastapi import FastAPI, Request
 import socket
-import mysql.connector
 from datetime import datetime
 from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
@@ -16,26 +16,15 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-# DB Connection
-db = mysql.connector.connect(
-    host="172.31.33.72",
-    user="root",
-    password="root",
-    database="hostlogs",
-    auth_plugin='caching_sha2_password'
-)
-
 @app.get("/hostname")
 def get_hostname(request: Request):
     hostname = socket.gethostname()
     ip = request.client.host
     timestamp = datetime.now()
 
-    cursor = db.cursor()
-    cursor.execute("INSERT INTO request_log (ip_address, hostname, timestamp) VALUES (%s, %s, %s)",
-                   (ip, hostname, timestamp))
-    db.commit()
-    cursor.close()
-
-    return {"hostname": hostname}
-
+    # Just return data, no DB insert
+    return {
+        "hostname": hostname,
+        "ip_address": ip,
+        "timestamp": timestamp.strftime("%Y-%m-%d %H:%M:%S")
+    }
